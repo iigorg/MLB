beed01e4c6c13dc455ed8a23b2f00ae9608f2e1b
unknown <igordiiuk@gmail.com>
2018-12-16 10:42:28 +0200

    first commit

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, Animated } from 'react-native';
 import { ListItem } from 'react-native-elements'
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
+
 import MLBGamedayApi from './../api/MLBGamedayApi'
 import { DateBar } from './../components/app'
 
+const AnimatedIcon = Animated.createAnimatedComponent(Icon)
 export default class GameScreen extends React.Component {
   static navigationOptions = {
     title: 'Games',
@@ -14,9 +17,14 @@ export default class GameScreen extends React.Component {
 
     this.state = {
       isLoading: false,
+      isExpanded: false,
       date: new Date(2018, 4, 24),
       games: [],
-    };    
+    }; 
+
+    // Setup animation values...
+    this.chevronRotation = new Animated.Value(0);
+    this.listItemHeight = new Animated.Value(0);
   }
 
   componentDidMount(){
@@ -43,12 +51,62 @@ export default class GameScreen extends React.Component {
     this.getGamesForDay();
   } 
 
+  onListItemPress = () => {
+    // Rotate the chevron...
+    let chevronToValue = (this.state.isExpanded) ? 0 : 1;   
+    Animated.timing(this.chevronRotation, {
+      toValue: chevronToValue, duration: 200
+    }).start();
+
+     // Expend/Collapse the list item...
+     let heightToValue = (this.state.isExpanded) ? 0 : 100;
+      Animated.timing(this.listItemHeight, {
+            toValue: heightToValue, duration: 200
+    }).start();
+
+     // Toggle the isExpended state...
+    this.setState({isExpanded: !this.state.isExpanded}); 
+  }
+
+  getChevronRotateStyle = () => {
+    // Setup the animated...
+    const rotate = this.chevronRotation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "90deg"],
+    });
+
+    //Return the style...
+    return {transform: [{ rotate: rotate}] };
+  }
+
+  renderSubtitle = () => {
+    return (
+      <Animated.View style={{height: this.listItemHeight}}>
+        <Text>Subtitle</Text>
+      </Animated.View>
+    )
+  }
+
   renderItem = ({item}) => {
   const title = item.awayTeamName 
+
+  const chevronStyle = this.getChevronRotateStyle()
+  const subtitle = this.renderSubtitle()
     return (       
         <ListItem
           key={item.key}
-          title={item.awayTeamName}          
+          title={item.awayTeamName}     
+          onPress={this.onListItemPress}  
+          subtitle={subtitle}
+
+          rightIcon={
+            <AnimatedIcon 
+              name="chevron-right" 
+              size={30} 
+              color="gray"
+              style={[ chevronStyle, { alignSelf: "flex-start" }]}
+            />
+          }   
         />
     )
   }  
